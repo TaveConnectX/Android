0bea38634e035ce117024221aa843aa57ec745d9
아딘컴\김아린 <jyandysys1@naver.com>
2023-05-22 16:30:38 +0900

    [FEAT] : Start 액티비티 생성

---
+++ app/src/main/java/com/example/taveconnect/Activity/StartActivity.kt
@@ -0,0 +1,12 @@
+package com.example.taveconnect.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.taveconnect.R
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
