e358547fb3292ad06a8e00e4d72378fd311c91b8
아딘컴\김아린 <jyandysys1@naver.com>
2023-05-25 14:41:01 +0900

    [FEAT] : HomeActivity에 Fragment 적용

--- app/src/main/java/com/example/taveconnect/HomeActivity.kt
+++ app/src/main/java/com/example/taveconnect/HomeActivity.kt
@@ -3,9 +3,9 @@ package com.example.taveconnect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ListActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list)
+        setContentView(R.layout.activity_home)
     }
 }
