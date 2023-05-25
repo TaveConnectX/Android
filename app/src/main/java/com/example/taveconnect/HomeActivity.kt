d89f128ad2ece72079865c00d6e17daa7d688787
아딘컴\김아린 <jyandysys1@naver.com>
2023-05-25 16:03:24 +0900

    [FEAT] : Menu 설정

--- app/src/main/java/com/example/taveconnect/HomeActivity.kt
+++ app/src/main/java/com/example/taveconnect/HomeActivity.kt
@@ -2,10 +2,16 @@ package com.example.taveconnect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.taveconnect.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
     }
 }
