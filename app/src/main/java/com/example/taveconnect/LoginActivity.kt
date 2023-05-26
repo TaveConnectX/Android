1c3d870677ad153a2c367652ed97f03e510b192f
sxmvx <rin6683@gmail.com>
2023-05-27 02:49:38 +0900

    [CHORE] 액티비티 연결, xml 수정

--- app/src/main/java/com/example/taveconnect/LoginActivity.kt
+++ app/src/main/java/com/example/taveconnect/LoginActivity.kt
@@ -1,13 +1,24 @@
 package com.example.taveconnect
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.taveconnect.R
 
-class LoginActivity : AppCompatActivity(){
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        //버튼 지정
+        val BtnEnjoy = findViewById<Button>(R.id.btn_enjoy)
+
+        //클릭리스너
+        BtnEnjoy.setOnClickListener {
+            val intent = Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
