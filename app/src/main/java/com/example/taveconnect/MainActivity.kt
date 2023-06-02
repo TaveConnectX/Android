4f9147ef9b4e7b00cdbcc2f4fc94e9f21b083732
아딘컴\김아린 <jyandysys1@naver.com>
2023-06-03 02:56:59 +0900

    [FEAT] : 카카오 로그인 로그 확인

--- app/src/main/java/com/example/taveconnect/MainActivity.kt
+++ app/src/main/java/com/example/taveconnect/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.taveconnect
 
+import android.content.ContentValues
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,7 +9,10 @@ import android.util.Log
 import android.widget.Button
 import com.example.taveconnect.R
 import com.example.taveconnect.databinding.ActivityMainBinding
+import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.util.Utility
+import com.kakao.sdk.user.UserApiClient
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -23,8 +28,34 @@ class MainActivity : AppCompatActivity() {
 
         //클릭리스너
         BtnKakao.setOnClickListener {
+
+            // 카카오 로그인
+
+            // 카카오계정으로 로그인 공통 callback 구성
+            // 카카오톡 로그인 불가 -> 카카오 계정으로 로그인 할 경우
+            val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+                if (error != null) {
+                    Log.e("KaKao", "카카오계정으로 로그인 실패", error)
+                } else if (token != null) {
+                    Log.i("KaKao", "카카오계정으로 로그인 성공 ${token.accessToken}")
+                }
+            }
+
+/*
+            // 카카오톡 설치 됨 -> 카카오 로그인, 설치 안됨 -> 카카오 계쩡으로 로그인
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
+                UserApiClient.instance.loginWithKakaoAccount(context) { token, error ->
+                    if (error != null) {
+                        Log.e("KaKao", "카카오톡으로 로그인 실패", error)
+                    }
+                }
+            } */
+
+
+
+/*
             val intent = Intent(this,LoginActivity::class.java)
-            startActivity(intent)
+            startActivity(intent)*/
         }
 
         // 키 해시 확인하기
