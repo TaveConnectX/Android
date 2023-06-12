a8a48abaa26ba0dc8d2375e4e9ea59f5ed74a082
아딘컴\김아린 <jyandysys1@naver.com>
2023-06-13 00:47:36 +0900

    [FEAT] : LoginActivity 닉네임 받아오기

--- app/src/main/java/com/example/taveconnect/LoginActivity.kt
+++ app/src/main/java/com/example/taveconnect/LoginActivity.kt
@@ -10,20 +10,42 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.taveconnect.R
 import com.kakao.sdk.user.UserApiClient
 import com.example.taveconnect.databinding.ActivityLoginBinding
+import okhttp3.internal.userAgent
 
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.tvName.text
+
+        loginInfo()
 
         //버튼 지정
         binding.btnEnjoy.setOnClickListener {
             val intent = Intent(this,HomeActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    // 닉네임 받아오기
+
+    private fun loginInfo() {
+
+        UserApiClient.instance.me { user, error ->
+            if (error != null) {
+                Log.e("LOGIN", "사용자 정보 요청 실패", error)
+            }
+
+            else if (user != null) {
+                Log.i("LOGIN", "사용자 정보 요청 성공")
+                binding.tvName.text = user.kakaoAccount?.profile?.nickname
+            }
+        }
+
     }
 }
