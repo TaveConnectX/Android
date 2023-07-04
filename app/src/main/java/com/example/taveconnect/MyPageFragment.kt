2ad9ca42cca49f2d464efc87d8710fb2a02ac0c5
arinming <jyandysys1@naver.com>
2023-07-04 16:21:58 +0900

    [FIX] : MyPage Fragment 수정 - Fragment 연결, 레이아웃 비율

--- app/src/main/java/com/example/taveconnect/MyPageFragment.kt
+++ app/src/main/java/com/example/taveconnect/MyPageFragment.kt
@@ -5,10 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.taveconnect.databinding.FragmentSettingBinding
+import com.example.taveconnect.databinding.FragmentMypageBinding
 
 class MyPageFragment : Fragment(R.layout.fragment_mypage) {
-    private var _binding: FragmentSettingBinding? = null
+    private var _binding: FragmentMypageBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -16,7 +16,7 @@ class MyPageFragment : Fragment(R.layout.fragment_mypage) {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentSettingBinding.inflate(inflater, container, false)
+        _binding = FragmentMypageBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
     }
