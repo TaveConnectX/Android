68fa126070d79ccf586011b41a98cf62800daa8e
sxmvx <98005347+sxmvx@users.noreply.github.com>
2023-07-04 14:28:42 +0900

    [FIX] : 오류 수정

--- app/src/main/java/com/example/taveconnect/BurgerActivity.kt
+++ app/src/main/java/com/example/taveconnect/BurgerActivity.kt
@@ -21,6 +21,24 @@ class BurgerActivity: AppCompatActivity() {
 
 
     private fun Fragment.changeFragment() {
+
+        val intent2 = Intent(activity, LoadActivity::class.java)
+        val gameActivity = activity as GameActivity
+        val col1 = gameActivity.col1
+        val col2 = gameActivity.col2
+        val col3 = gameActivity.col3
+        val col4 = gameActivity.col4
+        val col5 = gameActivity.col5
+        val col6 = gameActivity.col6
+        val col7 = gameActivity.col7
+        intent2.putExtra("col1", col1)
+        intent2.putExtra("col2", col2)
+        intent2.putExtra("col3", col3)
+        intent2.putExtra("col4", col4)
+        intent2.putExtra("col5", col5)
+        intent2.putExtra("col6", col6)
+        intent2.putExtra("col7", col7)
+
         manager.beginTransaction().replace(R.id.fv_burger, this).commit()
     }
 
--- app/src/main/java/com/example/taveconnect/MenuFragment.kt
+++ app/src/main/java/com/example/taveconnect/MenuFragment.kt
@@ -23,6 +23,7 @@ class MenuFragment: Fragment(R.layout.fragment_menu) {
     ): View? {
         _binding = FragmentMenuBinding.inflate(inflater, container, false)
         val view = binding.root
+
         return view
     }
 
@@ -32,23 +33,6 @@ class MenuFragment: Fragment(R.layout.fragment_menu) {
         binding.btnGoMain.setOnClickListener {
             val intent = Intent(activity, HomeActivity::class.java)
             startActivity(intent)
-
-            val intent2 = Intent(activity, LoadActivity::class.java)
-            val gameActivity = activity as GameActivity
-            val col1 = gameActivity.col1
-            val col2 = gameActivity.col2
-            val col3 = gameActivity.col3
-            val col4 = gameActivity.col4
-            val col5 = gameActivity.col5
-            val col6 = gameActivity.col6
-            val col7 = gameActivity.col7
-            intent2.putExtra("col1", col1)
-            intent2.putExtra("col2", col2)
-            intent2.putExtra("col3", col3)
-            intent2.putExtra("col4", col4)
-            intent2.putExtra("col5", col5)
-            intent2.putExtra("col6", col6)
-            intent2.putExtra("col7", col7)
         }
 
         binding.btnNewStart.setOnClickListener {
