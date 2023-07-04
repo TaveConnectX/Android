91b7fe633f2f912eb0b917f76a7cac067246276a
sxmvx <98005347+sxmvx@users.noreply.github.com>
2023-07-04 14:47:00 +0900

    [CHORE] : BroadcastReceiver

--- app/src/main/java/com/example/taveconnect/BurgerActivity.kt
+++ app/src/main/java/com/example/taveconnect/BurgerActivity.kt
@@ -1,7 +1,12 @@
 package com.example.taveconnect
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.taveconnect.databinding.ActivityBurgerBinding
@@ -22,22 +27,30 @@ class BurgerActivity: AppCompatActivity() {
 
     private fun Fragment.changeFragment() {
 
-        val intent2 = Intent(activity, LoadActivity::class.java)
-        val gameActivity = activity as GameActivity
-        val col1 = gameActivity.col1
-        val col2 = gameActivity.col2
-        val col3 = gameActivity.col3
-        val col4 = gameActivity.col4
-        val col5 = gameActivity.col5
-        val col6 = gameActivity.col6
-        val col7 = gameActivity.col7
-        intent2.putExtra("col1", col1)
-        intent2.putExtra("col2", col2)
-        intent2.putExtra("col3", col3)
-        intent2.putExtra("col4", col4)
-        intent2.putExtra("col5", col5)
-        intent2.putExtra("col6", col6)
-        intent2.putExtra("col7", col7)
+        class ButtonClickedReceiver : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                val intent2 = Intent(activity, LoadActivity::class.java)
+                val gameActivity = activity as GameActivity
+                val col1 = gameActivity.col1
+                val col2 = gameActivity.col2
+                val col3 = gameActivity.col3
+                val col4 = gameActivity.col4
+                val col5 = gameActivity.col5
+                val col6 = gameActivity.col6
+                val col7 = gameActivity.col7
+                intent2.putExtra("col1", col1)
+                intent2.putExtra("col2", col2)
+                intent2.putExtra("col3", col3)
+                intent2.putExtra("col4", col4)
+                intent2.putExtra("col5", col5)
+                intent2.putExtra("col6", col6)
+                intent2.putExtra("col7", col7)
+            }
+        }
+
+        val receiver = ButtonClickedReceiver()
+        val intentFilter = IntentFilter("ButtonClickedBroadcast")
+        registerReceiver(receiver, intentFilter)
 
         manager.beginTransaction().replace(R.id.fv_burger, this).commit()
     }
--- app/src/main/java/com/example/taveconnect/MenuFragment.kt
+++ app/src/main/java/com/example/taveconnect/MenuFragment.kt
@@ -1,11 +1,13 @@
 package com.example.taveconnect
 
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import com.example.taveconnect.databinding.FragmentMenuBinding
@@ -32,14 +34,15 @@ class MenuFragment: Fragment(R.layout.fragment_menu) {
 
         binding.btnGoMain.setOnClickListener {
             val intent = Intent(activity, HomeActivity::class.java)
-            startActivity(intent)
+            val intent2 = Intent("ButtonClickedBroadcast")
+            activity?.sendBroadcast(intent2)
+            activity?.startActivity(intent)
         }
 
         binding.btnNewStart.setOnClickListener {
             val intent = Intent(activity, DifficultyActivity::class.java)
             startActivity(intent)
         }
-
     }
 }
 
