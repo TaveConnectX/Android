c2b80df07d9aac972be5a5dfca89c0741147ef70
arinming <jyandysys1@naver.com>
2023-07-11 09:04:56 +0900

    [FEAT] : 백업.,.,

--- app/src/main/java/com/example/taveconnect/ReviewActivity.kt
+++ app/src/main/java/com/example/taveconnect/ReviewActivity.kt
@@ -21,20 +21,15 @@ class ReviewActivity : AppCompatActivity() {
     private var arr = Array(7) { Array(6) { 0 } }
     private var recommendationList : MutableList<Int> = mutableListOf()
 
+
+
     private var first : Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityReviewBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
-        gameReviewAPI()
-
-        binding.btnHint.setOnClickListener {
-            gameReviewAPI()
-        }
-        showBurger()
-
 
         val col1 = intent.getIntArrayExtra("r_col1") ?: IntArray(6)
         val col2 = intent.getIntArrayExtra("r_col2") ?: IntArray(6)
@@ -44,12 +39,60 @@ class ReviewActivity : AppCompatActivity() {
         val col6 = intent.getIntArrayExtra("r_col6") ?: IntArray(6)
         val col7 = intent.getIntArrayExtra("r_col7") ?: IntArray(6)
 
-        var arraysGame = convertTo2DArray(col1, col2, col3, col4, col5, col6, col7)
-
         val gameIndex = intent.getIntExtra("reIndex", 0)
-
+        Log.d("지금 인덱스", gameIndex.toString())
 
         var nowIndex = gameIndex
+
+        gameReviewAPI { listValues ->
+            if (nowIndex >= 0 && nowIndex < listValues.size) {
+                val row = listValues[nowIndex]
+                if (row.size >= 7) {
+                    val col1Row = row[0]
+                    val col2Row = row[1]
+                    val col3Row = row[2]
+                    val col4Row = row[3]
+                    val col5Row = row[4]
+                    val col6Row = row[5]
+                    val col7Row = row[6]
+
+                    col1Row.indices.forEach { index ->
+                        col1[index] = col1Row[index]
+                    }
+                    col2Row.indices.forEach { index ->
+                        col2[index] = col2Row[index]
+                    }
+                    col3Row.indices.forEach { index ->
+                        col3[index] = col3Row[index]
+                    }
+                    col4Row.indices.forEach { index ->
+                        col4[index] = col4Row[index]
+                    }
+                    col5Row.indices.forEach { index ->
+                        col5[index] = col5Row[index]
+                    }
+                    col6Row.indices.forEach { index ->
+                        col6[index] = col6Row[index]
+                    }
+                    col7Row.indices.forEach { index ->
+                        col7[index] = col7Row[index]
+                    }
+                }
+            } else {
+                nowIndex = 0
+                val row = listValues[nowIndex]
+
+            }
+
+            // col1부터 col7까지의 값이 변경되었음
+            Log.d("리스트1313914", "col1: ${col1.contentToString()}")
+            Log.d("리스트1313914", "col2: ${col2.contentToString()}")
+            Log.d("리스트1313914", "col3: ${col3.contentToString()}")
+            Log.d("리스트1313914", "col4: ${col4.contentToString()}")
+            Log.d("리스트1313914", "col5: ${col5.contentToString()}")
+            Log.d("리스트1313914", "col6: ${col6.contentToString()}")
+            Log.d("리스트1313914", "col7: ${col7.contentToString()}")
+        }
 
         if(nowIndex == gameIndex && nowIndex == 0)
             Toast.makeText(this, "이전 게임이 없습니다.", Toast.LENGTH_SHORT).show()
@@ -229,15 +272,10 @@ class ReviewActivity : AppCompatActivity() {
                     val packageName = packageName
                     val ivId = resources.getIdentifier(coord, "id", packageName)
                     val imageView = findViewById<ImageView>(ivId)
-                    if (col1[i] == nowIndex) {
-                        if (nowIndex % 2 == 0) {
-                            imageView.setImageResource(R.drawable.ic_white)
-                            binding.btnHint.isVisible = false
-                        } else {
-                            imageView.setImageResource(R.drawable.ic_black)
-                            binding.btnHint.isVisible = true
-                        }
-                        break
+                    if (col1[i] % 2 == 0 && col1[i] != 0) {
+                        imageView.setImageResource(R.drawable.ic_white)
+                    } else if (col1[i] % 2 == 1) {
+                        imageView.setImageResource(R.drawable.ic_black)
                     }
                 }
                 for (i in 0 until col2.size) {
@@ -245,15 +283,10 @@ class ReviewActivity : AppCompatActivity() {
                     val packageName = packageName
                     val ivId = resources.getIdentifier(coord, "id", packageName)
                     val imageView = findViewById<ImageView>(ivId)
-                    if (col2[i] == nowIndex) {
-                        if (nowIndex % 2 == 0) {
-                            imageView.setImageResource(R.drawable.ic_white)
-                            binding.btnHint.isVisible = false
-                        } else {
-                            imageView.setImageResource(R.drawable.ic_black)
-                            binding.btnHint.isVisible = true
-                        }
-                        break
+                    if (col2[i] % 2 == 0 && col1[i] != 0) {
+                        imageView.setImageResource(R.drawable.ic_white)
+                    } else if (col1[i] % 2 == 1) {
+                        imageView.setImageResource(R.drawable.ic_black)
                     }
                 }
                 for (i in 0 until col3.size) {
@@ -261,15 +294,10 @@ class ReviewActivity : AppCompatActivity() {
                     val packageName = packageName
                     val ivId = resources.getIdentifier(coord, "id", packageName)
                     val imageView = findViewById<ImageView>(ivId)
-                    if (col3[i] == nowIndex) {
-                        if (nowIndex % 2 == 0) {
-                            imageView.setImageResource(R.drawable.ic_white)
-                            binding.btnHint.isVisible = false
-                        } else {
-                            imageView.setImageResource(R.drawable.ic_black)
-                            binding.btnHint.isVisible = true
-                        }
-                        break
+                    if (col3[i] % 2 == 0 && col3[i] != 0) {
+                        imageView.setImageResource(R.drawable.ic_white)
+                    } else if (col3[i] % 2 == 1) {
+                        imageView.setImageResource(R.drawable.ic_black)
                     }
                 }
                 for (i in 0 until col4.size) {
@@ -277,15 +305,10 @@ class ReviewActivity : AppCompatActivity() {
                     val packageName = packageName
                     val ivId = resources.getIdentifier(coord, "id", packageName)
                     val imageView = findViewById<ImageView>(ivId)
-                    if (col4[i] == nowIndex) {
-                        if (nowIndex % 2 == 0) {
-                            imageView.setImageResource(R.drawable.ic_white)
-                            binding.btnHint.isVisible = false
-                        } else {
-                            imageView.setImageResource(R.drawable.ic_black)
-                            binding.btnHint.isVisible = true
-                        }
-                        break
+                    if (col4[i] % 2 == 0 && col4[i] != 0) {
+                        imageView.setImageResource(R.drawable.ic_white)
+                    } else if (col4[i] % 2 == 1) {
+                        imageView.setImageResource(R.drawable.ic_black)
                     }
                 }
                 for (i in 0 until col5.size) {
@@ -293,15 +316,10 @@ class ReviewActivity : AppCompatActivity() {
                     val packageName = packageName
                     val ivId = resources.getIdentifier(coord, "id", packageName)
                     val imageView = findViewById<ImageView>(ivId)
-                    if (col5[i] == nowIndex) {
-                        if (nowIndex % 2 == 0) {
-                            imageView.setImageResource(R.drawable.ic_white)
-                            binding.btnHint.isVisible = false
-                        } else {
-                            imageView.setImageResource(R.drawable.ic_black)
-                            binding.btnHint.isVisible = true
-                        }
-                        break
+                    if (col5[i] % 2 == 0 && col5[i] != 0) {
+                        imageView.setImageResource(R.drawable.ic_white)
+                    } else if (col5[i] % 2 == 1) {
+                        imageView.setImageResource(R.drawable.ic_black)
                     }
                 }
                 for (i in 0 until col6.size) {
@@ -309,15 +327,10 @@ class ReviewActivity : AppCompatActivity() {
                     val packageName = packageName
                     val ivId = resources.getIdentifier(coord, "id", packageName)
                     val imageView = findViewById<ImageView>(ivId)
-                    if (col6[i] == nowIndex) {
-                        if (nowIndex % 2 == 0) {
-                            imageView.setImageResource(R.drawable.ic_white)
-                            binding.btnHint.isVisible = false
-                        } else {
-                            imageView.setImageResource(R.drawable.ic_black)
-                            binding.btnHint.isVisible = true
-                        }
-                        break
+                    if (col6[i] % 2 == 0 && col6[i] != 0) {
+                        imageView.setImageResource(R.drawable.ic_white)
+                    } else if (col6[i] % 2 == 1) {
+                        imageView.setImageResource(R.drawable.ic_black)
                     }
                 }
                 for (i in 0 until col7.size) {
@@ -325,15 +338,10 @@ class ReviewActivity : AppCompatActivity() {
                     val packageName = packageName
                     val ivId = resources.getIdentifier(coord, "id", packageName)
                     val imageView = findViewById<ImageView>(ivId)
-                    if (col7[i] == nowIndex) {
-                        if (nowIndex % 2 == 0) {
-                            imageView.setImageResource(R.drawable.ic_white)
-                            binding.btnHint.isVisible = false
-                        } else {
-                            imageView.setImageResource(R.drawable.ic_black)
-                            binding.btnHint.isVisible = true
-                        }
-                        break
+                    if (col7[i] % 2 == 0 && col7[i] != 0) {
+                        imageView.setImageResource(R.drawable.ic_white)
+                    } else if (col7[i] % 2 == 1) {
+                        imageView.setImageResource(R.drawable.ic_black)
                     }
                 }
 
@@ -442,30 +450,26 @@ class ReviewActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
-
-    fun gameReviewAPI() {
+    fun gameReviewAPI(callback: (MutableList<List<List<Int>>>) -> Unit) {
         val gameAPI = RetrofitClient.getInstance().create(RetroiftAPI::class.java)
 
-        gameAPI.getGameReview().enqueue(object: Callback<GameReviewData> {
+        gameAPI.getGameReview().enqueue(object : Callback<GameReviewData> {
             override fun onResponse(call: Call<GameReviewData>, response: Response<GameReviewData>) {
                 if (response.isSuccessful) {
                     response.body()?.let { gameReviewData ->
-                        val firstList: MutableList<Int> = mutableListOf()
+                        val listValues: MutableList<List<List<Int>>> = mutableListOf()
 
-                        // 받아온 데이터를 순회하면서 recommendation 값과 first 값을 추출하여 리스트에 추가
+                        // 받아온 데이터를 순회하면서 list 값을 추출하여 리스트에 추가
                         for (gameReviewDataItem in gameReviewData) {
-                            recommendationList.add(gameReviewDataItem.recommendation)
-                            firstList.add(gameReviewDataItem.first)
+                            val listValue = gameReviewDataItem.list
+                            listValues.add(listValue)
                         }
 
+                        // 추출한 list 값들을 확인
+                        Log.d("ListValues", listValues.toString())
 
-                        // 추출한 recommendation 값들을 확인
-                        Log.d("RecommendationList", recommendationList.toString())
-                        // 추출한 first 값들을 확인
-                        Log.d("FirstList", firstList[0].toString())
-
-                        GlobalApplication.prefs.setInt("first", firstList[0])
-
+                        // listValues를 이용한 작업을 여기에서 수행
+                        callback(listValues)
                     }
                     Log.d("GameReviewAPI", "성공 ${response.body().toString()}")
                 }
@@ -476,7 +480,6 @@ class ReviewActivity : AppCompatActivity() {
             }
         })
     }
-
 
 
     fun convertTo2DArray(col1: IntArray, col2: IntArray, col3: IntArray, col4: IntArray, col5: IntArray, col6: IntArray, col7: IntArray): Array<Array<Int>> {
@@ -520,5 +523,20 @@ class ReviewActivity : AppCompatActivity() {
         }
     }
 
+    fun processListValues(listValues: List<List<List<Int>>>): Array<Array<Int>> {
+        // listValues를 사용하여 필요한 작업을 수행
+        Log.d("리스트", listValues.toString())
+
+        val col1 = listValues[0].map { it[0] }.toIntArray()
+        val col2 = listValues[1].map { it[0] }.toIntArray()
+        val col3 = listValues[2].map { it[0] }.toIntArray()
+        val col4 = listValues[3].map { it[0] }.toIntArray()
+        val col5 = listValues[4].map { it[0] }.toIntArray()
+        val col6 = listValues[5].map { it[0] }.toIntArray()
+        val col7 = listValues[6].map { it[0] }.toIntArray()
+
+        // col1부터 col7까지 사용하여 필요한 작업을 수행
+        return convertTo2DArray(col1, col2, col3, col4, col5, col6, col7)
+    }
 
 }
